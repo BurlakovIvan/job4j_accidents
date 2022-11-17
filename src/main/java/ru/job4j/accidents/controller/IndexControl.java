2fa712a04fd6abcf48c40d55c68f87bb9999e247
BurlakovIvan <burlakovid@inbox.ru>
2022-11-17 22:35:39 +0300

    Загрузка данных в вид

--- src/main/java/ru/job4j/accidents/controller/IndexControl.java
+++ src/main/java/ru/job4j/accidents/controller/IndexControl.java
@@ -2,6 +2,7 @@ package ru.job4j.accidents.controller;
 
 import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexControl {
 
     @GetMapping("/index")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("user", "Petr Arsentev");
         return "index";
     }
 }
