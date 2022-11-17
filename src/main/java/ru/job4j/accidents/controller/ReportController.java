ab57d06187ca14cf4d1c6e9b4fbac0acca685abf
BurlakovIvan <burlakovid@inbox.ru>
2022-11-18 01:25:07 +0300

    IndexControl. Таблица и вид. Заявления на отдельную страницу вынес

--- src/main/java/ru/job4j/accidents/controller/ReportController.java
+++ src/main/java/ru/job4j/accidents/controller/ReportController.java
@@ -5,20 +5,18 @@ import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.job4j.accidents.service.AccidentService;
 import ru.job4j.accidents.service.ReportService;
 
 @Controller
 @ThreadSafe
 @AllArgsConstructor
-public class IndexControl {
+public class ReportController {
+    private final ReportService reportService;
 
-    private final AccidentService accidentService;
-
-    @GetMapping("/index")
-    public String index(Model model) {
+    @GetMapping("/reports")
+    public String reports(Model model) {
         model.addAttribute("user", "Petr Arsentev");
-        model.addAttribute("accidents", accidentService.findAll().values());
-        return "index";
+        model.addAttribute("reports", reportService.findAll().values());
+        return "reports";
     }
 }
