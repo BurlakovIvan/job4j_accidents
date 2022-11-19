0dae9bcfb184a2a4833ca6d17c1ed6b468fcd5c3
BurlakovIvan <burlakovid@inbox.ru>
2022-11-19 21:23:11 +0300

    Form с агрегационными объектами. Типы и правила переделал на использование Map

--- src/main/java/ru/job4j/accident/repository/AccidentMem.java
+++ src/main/java/ru/job4j/accident/repository/AccidentMem.java
@@ -14,43 +14,51 @@ import java.util.concurrent.atomic.AtomicInteger;
 @ThreadSafe
 public class AccidentMem {
     private final Map<Integer, Accident> accidents = new ConcurrentHashMap<>();
-    private final List<AccidentType> types = new ArrayList<>();
-    private final List<Rule> rules = new ArrayList<>();
-    private final AtomicInteger ids = new AtomicInteger(0);
+    private final Map<Integer, AccidentType> types = new ConcurrentHashMap<>();
+    private final Map<Integer, Rule> rules = new ConcurrentHashMap<>();
+    private final AtomicInteger idAccident = new AtomicInteger(0);
+    private final AtomicInteger idType = new AtomicInteger(0);
+    private final AtomicInteger idRule = new AtomicInteger(0);
 
     public AccidentMem() {
-        types.add(new AccidentType(1, "Две машины"));
-        types.add(new AccidentType(2, "Машина и человек"));
-        types.add(new AccidentType(3, "Машина и велосипед"));
+        int id = idType.incrementAndGet();
+        types.put(id, new AccidentType(id, "Две машины"));
+        id = idType.incrementAndGet();
+        types.put(id, new AccidentType(id, "Машина и человек"));
+        id = idType.incrementAndGet();
+        types.put(id, new AccidentType(id, "Машина и велосипед"));
 
-        rules.add(new Rule(1, "Статья. 1"));
-        rules.add(new Rule(2, "Статья. 2"));
-        rules.add(new Rule(3, "Статья. 3"));
+        id = idRule.incrementAndGet();
+        rules.put(id, new Rule(id, "Статья. 1"));
+        id = idRule.incrementAndGet();
+        rules.put(id, new Rule(id, "Статья. 2"));
+        id = idRule.incrementAndGet();
+        rules.put(id, new Rule(id, "Статья. 3"));
 
-        int id = ids.incrementAndGet();
+        id = idAccident.incrementAndGet();
         accidents.put(id,
                 new Accident(id, "Accident1", "Description1",
-                        "Address1", types.get(0), Set.of(rules.get(0), rules.get(2))));
-        id = ids.incrementAndGet();
+                        "Address1", types.get(1), Set.of(rules.get(1), rules.get(3))));
+        id = idAccident.incrementAndGet();
         accidents.put(id,
                 new Accident(id, "Accident2", "Description2",
-                        "Address2", types.get(2), Set.of(rules.get(1), rules.get(2))));
-        id = ids.incrementAndGet();
+                        "Address2", types.get(3), Set.of(rules.get(2), rules.get(3))));
+        id = idAccident.incrementAndGet();
         accidents.put(id,
                 new Accident(id, "Accident3", "Description3",
-                        "Address3", types.get(1), Set.of(rules.get(0))));
-        id = ids.incrementAndGet();
+                        "Address3", types.get(2), Set.of(rules.get(1))));
+        id = idAccident.incrementAndGet();
         accidents.put(id,
                 new Accident(id, "Accident4", "Description4",
-                        "Address4", types.get(0), Set.of(rules.get(2), rules.get(1))));
-        id = ids.incrementAndGet();
+                        "Address4", types.get(1), Set.of(rules.get(3), rules.get(2))));
+        id = idAccident.incrementAndGet();
         accidents.put(id,
                 new Accident(id, "Accident5", "Description5",
-                        "Address5", types.get(1), Set.of(rules.get(0), rules.get(2))));
-        id = ids.incrementAndGet();
+                        "Address5", types.get(2), Set.of(rules.get(1), rules.get(3))));
+        id = idAccident.incrementAndGet();
         accidents.put(id,
                 new Accident(id, "Accident6", "Description6",
-                        "Address6", types.get(2), Set.of(rules.get(1), rules.get(2))));
+                        "Address6", types.get(3), Set.of(rules.get(2), rules.get(3))));
     }
 
     public List<Accident> findAll() {
@@ -58,30 +66,30 @@ public class AccidentMem {
     }
 
     public List<AccidentType> findAllAccidentType() {
-        return new ArrayList<>(types);
+        return types.values().stream().toList();
     }
 
     public List<Rule> findAllRule() {
-        return new ArrayList<>(rules);
+        return rules.values().stream().toList();
     }
 
     public Set<Rule> findRule(List<Integer> rulesList) {
         Set<Rule> result = new HashSet<>();
         for (int id : rulesList) {
-            result.add(rules.get(id - 1));
+            result.add(rules.get(id));
         }
         return result;
     }
 
     public void create(Accident accident) {
-        int id = ids.incrementAndGet();
-        accident.setType(types.get(accident.getType().getId() - 1));
+        int id = idAccident.incrementAndGet();
+        accident.setType(types.get(accident.getType().getId()));
         accident.setId(id);
         accidents.put(id, accident);
     }
 
     public void update(Accident accident) {
-        accident.setType(types.get(accident.getType().getId() - 1));
+        accident.setType(types.get(accident.getType().getId()));
         accidents.replace(accident.getId(), accident);
     }
 
