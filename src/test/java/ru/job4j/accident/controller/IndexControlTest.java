e6fe9c0c0da7179f9eeb746c52a7ec3cdb68bf8d
BurlakovIvan <burlakovid@inbox.ru>
2022-12-02 01:24:07 +0300

    Spring test. RunWith

--- src/test/java/ru/job4j/accident/controller/IndexControlTest.java
+++ src/test/java/ru/job4j/accident/controller/IndexControlTest.java
@@ -7,13 +7,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.Test;
 
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.accident.Main;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
 class IndexControlTest {
